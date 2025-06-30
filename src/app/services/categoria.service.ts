777d58b0705037df410886325c252741e6dd2c17
narciav <107314945+narciav@users.noreply.github.com>
2025-06-30 05:01:27 -0600

    feat: enhance categoria list and service for improved data handling and pagination

--- src/app/services/categoria.service.ts
+++ src/app/services/categoria.service.ts
@@ -13,11 +13,19 @@ export class CategoriaService extends BaseService<ICategoria> {
   private alertService: AlertService = inject(AlertService);
 
 getAll() {
-  this.findAllWithParams({ page: this.search.page, size: this.search.size }).subscribe({
+  this.findAllWithParams({ page: this.search.page || 1, size: this.search.size || 5 }).subscribe({
     next: (response: any) => {
-      this.search = { ...this.search, ...response.meta };
-      this.totalItems = Array.from({ length: this.search.totalPages ? this.search.totalPages : 0 }, (_, i) => i + 1);
-      this.categoriaListSignal.set(response.data); // <-- Aquí debe estar el array de categorías
+      const categorias = Array.isArray(response) ? response : response.data;
+      this.categoriaListSignal.set(categorias);
+      if (response.meta) {
+        this.search = {
+          ...this.search,
+          ...response.meta,
+          page: Number(response.meta?.page) || 1,
+          size: Number(response.meta?.size) || 5
+        };
+        this.totalItems = Array.from({ length: this.search.totalPages ? this.search.totalPages : 0 }, (_, i) => i + 1);
+      }
     }
   });
 }
